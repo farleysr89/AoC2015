f1d68f242b9bee986f23750608e45ad3567ed858
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-09 18:38:30 -0600

    Day04 Part1 Complete

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Day04
 {
@@ -14,11 +16,42 @@ namespace Day04
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            int i = 0;
+            while (true)
+            {
+                string tmp = MD5Hash(_input + i.ToString());
+                if (tmp.Substring(0, 5) == "00000") break;
+                i++;
+            }
+            Console.WriteLine("Answer is " + i);
+
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+        }
+
+        // Function taken from here: https://www.godo.dev/tutorials/csharp-md5/
+        public static string MD5Hash(string text)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+
+            //compute hash from the bytes of text  
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(text));
+
+            //get hash result after compute it  
+            byte[] result = md5.Hash;
+
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                //change it into 2 hexadecimal digits  
+                //for each byte  
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+
+            return strBuilder.ToString();
         }
     }
 }
