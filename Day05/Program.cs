803f2dc83d7f0bd52710209b21ebb8bca801daab
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-09 19:19:18 -0600

    Day05 Part1 Complete

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day05
 {
@@ -14,6 +17,36 @@ namespace Day05
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> strings = _input.Split('\n').ToList();
+            List<string> niceStrings = new List<string>();
+            string vowels = "aeiou";
+            string[] badPairs = new string[]
+            {
+                "ab",
+                "cd",
+                "pq",
+                "xy"
+            };
+            // Regex from here: https://stackoverflow.com/questions/2564092/net-regular-expression-for-n-number-of-consecutive-characters
+            string reg = @"\S*(.)\1\S*";
+            foreach (var s in strings)
+            {
+                bool bad = false;
+                foreach(var b in badPairs)
+                {
+                    if (s.Contains(b))
+                    {
+                        bad = true;
+                        break;
+                    }
+                }
+                if (bad) continue;
+                MatchCollection matches = Regex.Matches(s, reg);
+                if (!matches.Any()) continue;
+                if (s.Where(c => vowels.Contains(c)).Count() < 3) continue;
+                niceStrings.Add(s);
+            }
+            Console.WriteLine("Nice strings = " + niceStrings.Count());
         }
 
         static void SolvePart2()
@@ -22,3 +55,4 @@ namespace Day05
         }
     }
 }
+
