3823b23fd8d79bed1861ae79b6bff89b83f79b3b
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-11 17:36:42 -0600

    Day 8 Part 1 done

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Day08
 {
@@ -14,6 +17,16 @@ namespace Day08
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            int total = 0;
+            int newTotal = 0;
+            foreach(var s in data)
+            {
+                total += s.Length;
+                var a = Regex.Unescape(s);
+                newTotal += a.Length - 2;
+            }
+            Console.WriteLine("Difference = " + (total - newTotal));
         }
 
         static void SolvePart2()
