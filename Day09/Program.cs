b276d6bc848833bdb32537f800d365174766c465
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-12 11:32:40 -0600

    Day 9 Part 2 Complete

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -51,6 +51,36 @@ namespace Day09
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            List<Route> routes = new List<Route>();
+            HashSet<string> cities = new HashSet<string>();
+            foreach (var s in data)
+            {
+                var x = s.Split(" ");
+                var r = new Route { dist = int.Parse(x[4]) };
+                r.ends[0] = x[0];
+                r.ends[1] = x[2];
+                cities.Add(x[0]);
+                cities.Add(x[2]);
+                routes.Add(r);
+            }
+            var sortedRoutes = routes.OrderByDescending(x => x.dist);
+            int maxDist = int.MinValue;
+            foreach (var r in sortedRoutes)
+            {
+                HashSet<string> visited = new HashSet<string>
+                {
+                    r.ends[0],
+                    r.ends[1]
+                };
+                var tmpRoutes = new List<Route>(sortedRoutes);
+                tmpRoutes.Remove(r);
+                int tmp = CheckMaxRoute(tmpRoutes, visited, cities, r.ends[1], r.dist);
+                if (tmp > maxDist) maxDist = tmp;
+                tmp = CheckMaxRoute(tmpRoutes, visited, cities, r.ends[0], r.dist);
+                if (tmp > maxDist) maxDist = tmp;
+            }
+            Console.WriteLine("Max distance = " + maxDist);
         }
 
         public static int CheckRoute(List<Route> routes, HashSet<string> visited, HashSet<string> cities, string last, int dist)
@@ -71,6 +101,25 @@ namespace Day09
                 if (tmp < minDist) minDist = tmp;
             }
             return minDist;
+        }
+        public static int CheckMaxRoute(List<Route> routes, HashSet<string> visited, HashSet<string> cities, string last, int dist)
+        {
+            if (visited.Count == cities.Count) return dist;
+
+            int maxDist = int.MinValue;
+            foreach (var r in routes.Where(x => x.ends.Contains(last) && (!visited.Contains(x.ends[1]) || !visited.Contains(x.ends[0]))))
+            {
+                HashSet<string> tmpVisited = new HashSet<string>(visited)
+                {
+                    r.ends[1],
+                    r.ends[0]
+                };
+                var tmpRoutes = new List<Route>(routes);
+                tmpRoutes.Remove(r);
+                int tmp = CheckMaxRoute(tmpRoutes, tmpVisited, cities, last == r.ends[1] ? r.ends[0] : r.ends[1], dist + r.dist);
+                if (tmp > maxDist) maxDist = tmp;
+            }
+            return maxDist;
         }
     }
     class Route
