091cf06cccba8f8a88506ed8e36d8735f932910c
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-12 12:08:11 -0600

    Day 10 Part 2 Complete

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Day10
 {
@@ -7,77 +8,52 @@ namespace Day10
     {
         static void Main(string[] args)
         {
-            SolvePart1();
-            SolvePart2();
+            var s = SolvePart1();
+            SolvePart2(s);
         }
 
-        static void SolvePart1()
+        static string SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
-            Console.WriteLine(countnndSay(_input, 40).Length);
-        }
-
-        static void SolvePart2()
-        {
-            string _input = File.ReadAllText("Input.txt");
-        }
-
-        // Returns n'th term in  
-        // look-and-say sequence 
-        // Taken from here: https://www.geeksforgeeks.org/look-and-say-sequence/
-        static string countnndSay(string str, int n)
-        {
-
-            // Find n'th term by generating  
-            // all terms from 3 to n-1.  
-            // Every term is generated using 
-            // previous term 
-            for (int i = 1; i <= n; i++)
+            for (int i = 1; i <= 40; i++)
             {
-                // In below for loop, previous  
-                // character is processed in  
-                // current iteration. That is  
-                // why a dummy character is  
-                // added to make sure that loop 
-                // runs one extra iteration. 
-                str += '$';
-                int len = str.Length;
-
-                int cnt = 1; // Initialize count of 
-                             // matching chars 
-                string tmp = ""; // Initialize i'th  
-                                 // term in series 
-                char[] arr = str.ToCharArray();
-
-                // Process previous term  
-                // to find the next term 
-                for (int j = 1; j < len; j++)
-                {
-                    // If current character 
-                    // does't match 
-                    if (arr[j] != arr[j - 1])
-                    {
-                        // Append count of  
-                        // str[j-1] to temp 
-                        tmp += cnt + 0;
-
-                        // Append str[j-1] 
-                        tmp += arr[j - 1];
-
-                        // Reset count 
-                        cnt = 1;
-                    }
-
-                    // If matches, then increment  
-                    // count of matching characters 
-                    else cnt++;
-                }
-
-                // Update str 
-                str = tmp;
+                _input = lookandsay(_input);
             }
+            Console.WriteLine("Part 1 " + _input.Length);
+            return _input;
+        }
 
-            return str;
+        static void SolvePart2(string s)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                s = lookandsay(s);
+            }
+            Console.WriteLine("Part 2 " + s.Length);
+        }
+
+        static string lookandsay(string number)
+        {
+            StringBuilder result = new StringBuilder();
+
+            char repeat = number[0];
+            number = number.Substring(1, number.Length - 1) + " ";
+            int times = 1;
+
+            foreach (char actual in number)
+            {
+                if (actual != repeat)
+                {
+                    result.Append(Convert.ToString(times) + repeat);
+                    times = 1;
+                    repeat = actual;
+                }
+                else
+                {
+                    times += 1;
+                }
+            }
+            return result.ToString();
         }
     }
 }
