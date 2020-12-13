5fda8d924f4b9ec4be4d776a5dd565a921694e9d
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-13 11:28:20 -0600

    Day 12 Complete

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.IO;
+using System.Linq;
 
 namespace Day12
 {
@@ -14,11 +17,24 @@ namespace Day12
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            var j = (JArray)JsonConvert.DeserializeObject(_input);
+            Console.WriteLine("sum of part 1 = " + Calculate(j, null));
+
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            var j = (JArray)JsonConvert.DeserializeObject(_input);
+            Console.WriteLine("Sum of part 2 = " + Calculate(j, "red"));
         }
+
+        static long Calculate(JObject o, string filter)
+        {
+            if (o.Properties().Select(x => x.Value).OfType<JValue>().Select(x => x.Value).Contains(filter)) return 0;
+            return o.Properties().Sum((dynamic a) => (long)Calculate(a.Value, filter));
+        }
+        static long Calculate(JArray arr, string filter) => arr.Sum((Func<dynamic, long>)((dynamic a) => (long)Calculate(a, filter)));
+        static long Calculate(JValue val, string filter) => val.Type == JTokenType.Integer ? (long)val.Value : 0;
     }
 }
