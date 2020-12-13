65c04a4a95666e52ee1cc5d4fd3ce523a4f43940
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-13 11:37:53 -0600

    More Day 13 Setup

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Day13
@@ -14,11 +15,26 @@ namespace Day13
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<Relation> relations = new List<Relation>();
+            foreach (var s in _input.Split('\n'))
+            {
+                var l = s.Split(" ");
+                bool gain = l[2] == "gain";
+                var r = new Relation { name1 = l[0], name2 = l[10], happiness = gain ? int.Parse(l[3]) : int.Parse(l[3]) * -1 };
+                relations.Add(r);
+            }
+            Console.WriteLine();
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
         }
+    }
+    class Relation
+    {
+        public string name1;
+        public string name2;
+        public int happiness;
     }
 }
