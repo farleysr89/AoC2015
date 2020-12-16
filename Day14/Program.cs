3fb6693f12f2b5a4eefdbb28114c3a0900fe8d71
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-16 17:48:47 -0600

    Day 14 Part 2 Complete

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -28,12 +28,30 @@ namespace Day14
                 reindeers.Add(new Reindeer { name = name, speed = int.Parse(speed), flyTime = int.Parse(flyTime), restTime = int.Parse(restTime) });
             }
             var sorted = reindeers.OrderByDescending(x => GetDistance(x));
-            Console.WriteLine("Winning reindeer traveled " + GetDistance(sorted.First()) + "km");
+            Console.WriteLine("Winning reindeer traveled " + GetDistance(sorted.First()) + " km");
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            List<Reindeer> reindeers = new List<Reindeer>();
+            foreach (var s in data)
+            {
+                var l = s.Split(" ");
+                var name = l[0];
+                var speed = l[3];
+                var flyTime = l[6];
+                var restTime = l[13];
+                reindeers.Add(new Reindeer { name = name, speed = int.Parse(speed), flyTime = int.Parse(flyTime), restTime = int.Parse(restTime) });
+            }
+            for (int i = 1; i <= 2503; i++)
+            {
+                var max = reindeers.Max(r => GetDistance(r, i));
+                foreach (var r in reindeers.Where(r => GetDistance(r, i) == max)) r.score++;
+            }
+            var sorted = reindeers.OrderByDescending(x => x.score);
+            Console.WriteLine("Winning reindeer got " + sorted.First().score + " points");
         }
 
         static int GetDistance(Reindeer r)
@@ -43,6 +61,13 @@ namespace Day14
             int leftoverSeconds = 2503 - r.TotalTime() * fullCycles;
             return dist + (leftoverSeconds < r.flyTime ? leftoverSeconds * r.speed : r.flyTime * r.speed);
         }
+        static int GetDistance(Reindeer r, int time)
+        {
+            int fullCycles = time / r.TotalTime();
+            int dist = r.speed * fullCycles * r.flyTime;
+            int leftoverSeconds = time - r.TotalTime() * fullCycles;
+            return dist + (leftoverSeconds < r.flyTime ? leftoverSeconds * r.speed : r.flyTime * r.speed);
+        }
     }
     class Reindeer
     {
@@ -50,6 +75,7 @@ namespace Day14
         public int speed;
         public int flyTime;
         public int restTime;
+        public int score = 0;
         public int TotalTime()
         {
             return flyTime + restTime;
