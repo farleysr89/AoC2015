b439e53cc2c1c0d24f20e83648865f523e466c25
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-15 18:34:45 -0600

    Day 14 Part 1 Complete

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day14
 {
@@ -14,11 +16,43 @@ namespace Day14
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            List<Reindeer> reindeers = new List<Reindeer>();
+            foreach (var s in data)
+            {
+                var l = s.Split(" ");
+                var name = l[0];
+                var speed = l[3];
+                var flyTime = l[6];
+                var restTime = l[13];
+                reindeers.Add(new Reindeer { name = name, speed = int.Parse(speed), flyTime = int.Parse(flyTime), restTime = int.Parse(restTime) });
+            }
+            var sorted = reindeers.OrderByDescending(x => GetDistance(x));
+            Console.WriteLine("Winning reindeer traveled " + GetDistance(sorted.First()) + "km");
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+        }
+
+        static int GetDistance(Reindeer r)
+        {
+            int fullCycles = 2503 / r.TotalTime();
+            int dist = r.speed * fullCycles * r.flyTime;
+            int leftoverSeconds = 2503 - r.TotalTime() * fullCycles;
+            return dist + (leftoverSeconds < r.flyTime ? leftoverSeconds * r.speed : r.flyTime * r.speed);
+        }
+    }
+    class Reindeer
+    {
+        public string name;
+        public int speed;
+        public int flyTime;
+        public int restTime;
+        public int TotalTime()
+        {
+            return flyTime + restTime;
         }
     }
 }
