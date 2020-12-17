38b962f628387014044dc7d7861a04c1eac8e8c5
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-16 20:21:52 -0600

    Day 17 Part 2 Complete

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -7,7 +7,7 @@ namespace Day17
 {
     class Program
     {
-        static int eggnog = 150;
+        static readonly int eggnog = 150;
         static void Main(string[] args)
         {
             SolvePart1();
@@ -28,12 +28,30 @@ namespace Day17
                 x++;
             }
             Console.WriteLine("Total combinations = " + count);
-
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<int> containers = _input.Split('\n').Select(i => int.Parse(i)).ToList();
+            int count = 0;
+            var sorted = containers.OrderByDescending(x => x).ToList();
+            int x = 1;
+            int minCount = int.MaxValue;
+            foreach (var i in sorted)
+            {
+                var tmpList = new List<int>(sorted.Skip(x));
+                minCount = Math.Min(minCount, TrySolveMin(eggnog - i, tmpList, 1));
+                x++;
+            }
+            x = 1;
+            foreach (var i in sorted)
+            {
+                var tmpList = new List<int>(sorted.Skip(x));
+                count += TrySolveCountMin(eggnog - i, tmpList, 1, minCount);
+                x++;
+            }
+            Console.WriteLine("Total combinations of min containers = " + count);
         }
         static int TrySolve(int remaining, List<int> containers)
         {
@@ -46,6 +64,39 @@ namespace Day17
                 {
                     var tmpList = new List<int>(containers.Skip(x));
                     count += TrySolve(remaining - i, tmpList);
+                }
+                x++;
+            }
+            return count;
+        }
+        static int TrySolveMin(int remaining, List<int> containers, int conCount)
+        {
+            if (remaining == 0) return conCount;
+            int minCount = int.MaxValue;
+            int x = 1;
+            foreach (var i in containers)
+            {
+                if (i <= remaining)
+                {
+                    var tmpList = new List<int>(containers.Skip(x));
+                    minCount = Math.Min(minCount, TrySolveMin(remaining - i, tmpList, conCount + 1));
+                }
+                x++;
+            }
+            return minCount;
+        }
+        static int TrySolveCountMin(int remaining, List<int> containers, int conCount, int minCount)
+        {
+            if (conCount > minCount) return 0;
+            if (remaining == 0) return conCount == minCount ? 1 : 0;
+            int count = 0;
+            int x = 1;
+            foreach (var i in containers)
+            {
+                if (i <= remaining)
+                {
+                    var tmpList = new List<int>(containers.Skip(x));
+                    count += TrySolveCountMin(remaining - i, tmpList, conCount + 1, minCount);
                 }
                 x++;
             }
