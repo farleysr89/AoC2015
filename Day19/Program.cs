4b87fb426c2ff2950524b4328c1b575a02266815
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-17 18:53:51 -0600

    More Day 19 Setup

--- Day19/Program.cs
+++ Day19/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day19
 {
@@ -14,11 +16,38 @@ namespace Day19
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            bool last = false;
+            List<Rule> rules = new List<Rule>();
+            string compound = "";
+            foreach (var s in data)
+            {
+                if (s == "")
+                {
+                    last = true;
+                    continue;
+                }
+                if (last)
+                {
+                    compound = s;
+                }
+                else
+                {
+                    var r = s.Split(" => ");
+                    rules.Add(new Rule { input = r[0], output = r[1] });
+                }
+            }
+            Console.WriteLine();
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
         }
+    }
+    class Rule
+    {
+        public string input;
+        public string output;
     }
 }
