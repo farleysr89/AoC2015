cd4e753f8ecee81c5ff7a2b61fa6842a2f1c0b30
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-17 18:01:47 -0600

    Day 18 Part 1 Complete

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day18
 {
@@ -14,11 +16,61 @@ namespace Day18
         static void SolvePart1()
         {
             string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            bool[,] lights = new bool[100, 100];
+            int x = 0, y = 0;
+            foreach (var s in data)
+            {
+                if (s == "") continue;
+                foreach (var c in s)
+                {
+                    lights[x, y] = c == '#' ? true : false;
+                    x++;
+                }
+                y++;
+                x = 0;
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                bool[,] newLights = (bool[,])lights.Clone();
+                for (x = 0; x < 100; x++)
+                {
+                    for (y = 0; y < 100; y++)
+                    {
+                        int count = CheckNeighbors(x, y, lights);
+                        if (lights[x, y] && count != 2 && count != 3) newLights[x, y] = false;
+                        if (!lights[x, y] && count == 3) newLights[x, y] = true;
+                    }
+                }
+
+
+                lights = (bool[,])newLights.Clone();
+            }
+            int lightCount = 0;
+            foreach (var b in lights)
+            {
+                if (b) lightCount++;
+            }
+            Console.WriteLine("Lighted lights = " + lightCount);
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
+        }
+        static int CheckNeighbors(int x, int y, bool[,] lights)
+        {
+            int count = 0;
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                for (int h = y - 1; h <= y + 1; h++)
+                {
+                    if (i == x && h == y) continue;
+                    if (i < 0 || h < 0 || i > 99 || h > 99) continue;
+                    if (lights[i, h]) count++;
+                }
+            }
+            return count;
         }
     }
 }
