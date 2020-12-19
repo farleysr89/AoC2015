3417e7bf456151dc7199bf48c3d7f97a355cba9d
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-18 20:28:10 -0600

    Day 20 Part 1 Complete

---
+++ Day20/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolvePart1();
+            SolvePart2();
+        }
+
+        static void SolvePart1()
+        {
+            string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            int target = int.Parse(data[0]);
+            int[] houses = new int[3600000];
+            for (int i = 1; i <= 3600000; i++)
+            {
+                for (int j = i; j < 3600000; j += i)
+                {
+                    houses[j] += i * 10;
+                }
+            }
+            int min = 0;
+            int count = 0;
+            foreach (var i in houses)
+            {
+                if (i >= target)
+                {
+                    min = count;
+                    break;
+                }
+                count++;
+            }
+            Console.WriteLine("First house is " + min);
+        }
+
+        static void SolvePart2()
+        {
+            string _input = File.ReadAllText("Input.txt");
+            List<string> data = _input.Split('\n').ToList();
+            Console.WriteLine("");
+        }
+    }
+}
