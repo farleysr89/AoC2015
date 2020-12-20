2b46d991f35c29fccf94003b6bd1ff4a27c393e3
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-19 19:45:35 -0600

    Update Program.cs

--- Day21/Program.cs
+++ Day21/Program.cs
@@ -7,7 +7,7 @@ namespace Day21
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             SolvePart1();
             SolvePart2();
@@ -26,30 +26,36 @@ namespace Day21
                 if (l[0] == "Armor") armor = int.Parse(l[1]);
                 if (l[0] == "Damage") damage = int.Parse(l[1]);
             }
-            List<Weapon> weapons = new List<Weapon>();
-            weapons.Add(new Weapon { name = "Dagger", cost = 8, damage = 4 });
-            weapons.Add(new Weapon { name = "Shortsword", cost = 10, damage = 5 });
-            weapons.Add(new Weapon { name = "Warhammer", cost = 25, damage = 6 });
-            weapons.Add(new Weapon { name = "Longsword", cost = 40, damage = 7 });
-            weapons.Add(new Weapon { name = "Greataxe", cost = 74, damage = 8 });
+            List<Weapon> weapons = new List<Weapon>
+            {
+                new Weapon { name = "Dagger", cost = 8, damage = 4 },
+                new Weapon { name = "Shortsword", cost = 10, damage = 5 },
+                new Weapon { name = "Warhammer", cost = 25, damage = 6 },
+                new Weapon { name = "Longsword", cost = 40, damage = 7 },
+                new Weapon { name = "Greataxe", cost = 74, damage = 8 }
+            };
 
-            List<Armor> armors = new List<Armor>();
-            armors.Add(new Armor { name = "", cost = 0, armor = 0 });
-            armors.Add(new Armor { name = "Leather", cost = 13, armor = 1 });
-            armors.Add(new Armor { name = "Chainmail", cost = 31, armor = 2 });
-            armors.Add(new Armor { name = "Splintmail", cost = 53, armor = 3 });
-            armors.Add(new Armor { name = "Bandedmail", cost = 75, armor = 4 });
-            armors.Add(new Armor { name = "Platemail", cost = 102, armor = 5 });
+            List<Armor> armors = new List<Armor>
+            {
+                new Armor { name = "", cost = 0, armor = 0 },
+                new Armor { name = "Leather", cost = 13, armor = 1 },
+                new Armor { name = "Chainmail", cost = 31, armor = 2 },
+                new Armor { name = "Splintmail", cost = 53, armor = 3 },
+                new Armor { name = "Bandedmail", cost = 75, armor = 4 },
+                new Armor { name = "Platemail", cost = 102, armor = 5 }
+            };
 
-            List<Ring> rings = new List<Ring>();
-            rings.Add(new Ring { name = "", cost = 0, armor = 0 });
-            rings.Add(new Ring { name = "", cost = 0, armor = 0 });
-            rings.Add(new Ring { name = "Damage +1", cost = 25, damage = 1 });
-            rings.Add(new Ring { name = "Damage +2", cost = 50, damage = 2 });
-            rings.Add(new Ring { name = "Damage +3", cost = 100, damage = 3 });
-            rings.Add(new Ring { name = "Armor +1", cost = 20, armor = 1 });
-            rings.Add(new Ring { name = "Armor +2", cost = 40, armor = 2 });
-            rings.Add(new Ring { name = "Armor +3", cost = 80, armor = 3 });
+            List<Ring> rings = new List<Ring>
+            {
+                new Ring { name = "", cost = 0, armor = 0 },
+                new Ring { name = "", cost = 0, armor = 0 },
+                new Ring { name = "Damage +1", cost = 25, damage = 1 },
+                new Ring { name = "Damage +2", cost = 50, damage = 2 },
+                new Ring { name = "Damage +3", cost = 100, damage = 3 },
+                new Ring { name = "Armor +1", cost = 20, armor = 1 },
+                new Ring { name = "Armor +2", cost = 40, armor = 2 },
+                new Ring { name = "Armor +3", cost = 80, armor = 3 }
+            };
             Console.WriteLine("");
         }
 
