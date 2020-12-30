6b60dacefda337a44cdc3f1b14307c64e29d1a74
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-30 17:08:29 -0600

    Day 22 Part 2 Complete

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -33,8 +33,6 @@ namespace Day22
             int playerHP = 50;
             int mana = 500;
             int lowestMana = Fight(hp, damage, playerHP, mana, 0, 0, 0, 0, true, int.MaxValue);
-
-
             Console.WriteLine("Lowest Mana = " + lowestMana);
         }
 
@@ -42,12 +40,22 @@ namespace Day22
         {
             string _input = File.ReadAllText("Input.txt");
             List<string> data = _input.Split('\n').ToList();
-            Console.WriteLine("");
+            int hp = 0, damage = 0;
+            foreach (var s in data)
+            {
+                if (s == "") continue;
+                var l = s.Split(": ");
+                if (l[0] == "Hit Points") hp = int.Parse(l[1]);
+                if (l[0] == "Damage") damage = int.Parse(l[1]);
+            }
+            int playerHP = 50;
+            int mana = 500;
+            int lowestMana = Fight2(hp, damage, playerHP, mana, 0, 0, 0, 0, true, int.MaxValue);
+            Console.WriteLine("Lowest Mana = " + lowestMana);
         }
 
         static int Fight(int bossHP, int BossDamage, int playerHP, int playerMana, int shield, int poison, int recharge, int manaCost, bool playerTurn, int lowestCost)
         {
-            //int lowestCost = int.MaxValue;
             if (bossHP <= 0) return manaCost;
             if (playerHP <= 0) return -1;
             if (manaCost > lowestCost) return -1;
@@ -76,6 +84,46 @@ namespace Day22
                     if (shield > 2 && s.shield > 0) continue;
                     if (recharge > 2 && s.recharge > 0) continue;
                     int tmp = Fight(bossHP - s.damage, BossDamage, playerHP + s.heal, playerMana - s.cost, Math.Max(shield, s.shield), Math.Max(poison, s.poison), Math.Max(recharge, s.recharge), manaCost + s.cost, false, lowestCost);
+                    if (tmp != -1)
+                    {
+                        lowestCost = Math.Min(lowestCost, tmp);
+                    }
+                }
+            }
+            return lowestCost;
+        }
+        static int Fight2(int bossHP, int BossDamage, int playerHP, int playerMana, int shield, int poison, int recharge, int manaCost, bool playerTurn, int lowestCost)
+        {
+            if (bossHP <= 0) return manaCost;
+            if (playerHP <= 0) return -1;
+            if (manaCost > lowestCost) return -1;
+            if (!playerTurn)
+            {
+                playerHP -= shield > 0 ? Math.Max(1, BossDamage - 7) : BossDamage;
+                if (poison > 0) bossHP -= 3;
+                if (recharge > 0) playerMana += 101;
+                shield--;
+                poison--;
+                recharge--;
+                return Fight2(bossHP, BossDamage, playerHP, playerMana, shield, poison, recharge, manaCost, true, lowestCost);
+            }
+            else
+            {
+                playerHP--;
+                if (playerHP <= 0) return -1;
+                if (poison > 0) bossHP -= 3;
+                if (bossHP <= 0) return manaCost;
+                if (recharge > 0) playerMana += 101;
+                shield--;
+                poison--;
+                recharge--;
+                if (!spells.Any(x => x.cost <= playerMana)) return -1;
+                foreach (var s in spells.Where(x => x.cost <= playerMana))
+                {
+                    if (poison > 2 && s.poison > 0) continue;
+                    if (shield > 2 && s.shield > 0) continue;
+                    if (recharge > 2 && s.recharge > 0) continue;
+                    int tmp = Fight2(bossHP - s.damage, BossDamage, playerHP + s.heal, playerMana - s.cost, Math.Max(shield, s.shield), Math.Max(poison, s.poison), Math.Max(recharge, s.recharge), manaCost + s.cost, false, lowestCost);
                     if (tmp != -1)
                     {
                         lowestCost = Math.Min(lowestCost, tmp);
