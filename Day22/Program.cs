2f90b0d4b914d28f3c5b61cff82213731233417b
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-30 16:16:43 -0600

    Day 22 Setup

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -7,7 +7,12 @@ namespace Day22
 {
     class Program
     {
-        static void Main(string[] args)
+        static List<Spell> spells = new List<Spell> { new Spell { name = "Magic Missle", cost = 53, damage = 4 },
+                                                      new Spell { name = "Drain", cost = 73, damage = 2, heal = 2 },
+                                                      new Spell { name = "Shield", cost = 113, shield = 6 },
+                                                      new Spell { name = "Poison", cost = 173, poison = 6 },
+                                                      new Spell { name = "Recharge", cost = 229, recharge = 5 }};
+        static void Main()
         {
             SolvePart1();
             SolvePart2();
@@ -17,7 +22,20 @@ namespace Day22
         {
             string _input = File.ReadAllText("Input.txt");
             List<string> data = _input.Split('\n').ToList();
-            Console.WriteLine("");
+            int hp = 0, damage = 0;
+            foreach (var s in data)
+            {
+                if (s == "") continue;
+                var l = s.Split(": ");
+                if (l[0] == "Hit Points") hp = int.Parse(l[1]);
+                if (l[0] == "Damage") damage = int.Parse(l[1]);
+            }
+            int playerHP = 50;
+            int mana = 500;
+            int lowestMana = Fight(hp, damage, playerHP, mana, 0, 0, 0, 0);
+
+
+            Console.WriteLine("Lowest Mana = " + lowestMana);
         }
 
         static void SolvePart2()
@@ -26,5 +44,26 @@ namespace Day22
             List<string> data = _input.Split('\n').ToList();
             Console.WriteLine("");
         }
+
+        static int Fight(int bossHP, int BossDamage, int playerHP, int playerMana, int shield, int poison, int recharge, int manaCost)
+        {
+            if (bossHP <= 0) return manaCost;
+            if (playerHP <= 0) return -1;
+            foreach (var s in spells)
+            {
+
+            }
+            return manaCost;
+        }
+    }
+    public class Spell
+    {
+        public string name;
+        public int cost;
+        public int damage;
+        public int heal;
+        public int shield;
+        public int poison;
+        public int recharge;
     }
 }
