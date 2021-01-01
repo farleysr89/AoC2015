00b68876edefb24475973eb85b0a009d7fbbca0e
Farley <33631912+farleysr89@users.noreply.github.com>
2020-12-31 18:02:39 -0600

    Day 23 Part 2 Complete

--- Day23/Program.cs
+++ Day23/Program.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Day23
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             SolvePart1();
             SolvePart2();
@@ -71,14 +69,68 @@ namespace Day23
                 }
                 instruction = data[index];
             }
-            Console.WriteLine("b register is " + b);
+            Console.WriteLine("b register int part 1 is " + b);
         }
 
         static void SolvePart2()
         {
             string _input = File.ReadAllText("Input.txt");
-            List<string> data = _input.Split('\n').ToList();
-            Console.WriteLine("");
+            string[] data = _input.Split('\n');
+            int index = 0;
+            int a = 1, b = 0;
+            string instruction = data[0];
+            while (instruction != "")
+            {
+                var parts = instruction.Split(" ");
+                if (parts[0] == "hlf")
+                {
+                    if (parts[1] == "a") a /= 2;
+                    else if (parts[1] == "b") b /= 2;
+                    else
+                        Console.WriteLine("Something Broke!");
+                    index++;
+                }
+                else if (parts[0] == "tpl")
+                {
+                    if (parts[1] == "a") a *= 3;
+                    else if (parts[1] == "b") b *= 3;
+                    else
+                        Console.WriteLine("Something Broke!");
+                    index++;
+                }
+                else if (parts[0] == "inc")
+                {
+                    if (parts[1] == "a") a += 1;
+                    else if (parts[1] == "b") b += 1;
+                    else
+                        Console.WriteLine("Something Broke!");
+                    index++;
+                }
+                else if (parts[0] == "jmp")
+                {
+                    index += int.Parse(parts[1]);
+                }
+                else if (parts[0] == "jie")
+                {
+                    int x = int.Parse(parts[2]);
+                    if (parts[1] == "a," && a % 2 == 0) index += x;
+                    else if (parts[1] == "b," && b % 2 == 0) index += x;
+                    else index++;
+                }
+                else if (parts[0] == "jio")
+                {
+                    int x = int.Parse(parts[2]);
+                    if (parts[1] == "a," && a == 1) index += x;
+                    else if (parts[1] == "b," && b == 1) index += x;
+                    else index++;
+                }
+                else
+                {
+                    Console.WriteLine("Something Broke");
+                }
+                instruction = data[index];
+            }
+            Console.WriteLine("b register int part 2 is " + b);
         }
     }
 }
